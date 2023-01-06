de1078263f13f9d14de0e25b94e88b7a3f216a72
StutiLad <ladstuti18@gmail.com>
2023-01-06 16:01:17 +0530

    commit

--- Assignment-1/js/main.js
+++ Assignment-1/js/main.js
@@ -10,6 +10,13 @@ window.addEventListener('load', function () {
 
         responsive: [
             {
+                breakpoint: 100,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
                 breakpoint: 576,
                 settings: {
                     slidesToShow: 1,
@@ -39,7 +46,56 @@ window.addEventListener('load', function () {
             },
         ],
     })
-})
+}),
 
+    window.addEventListener('load', function () {
 
+        new Glider(document.querySelector(".about-glider"), {
+            slidesToShow: 6,
+            draggable: true,
+            arrows: {
+                prev: ".glider-prev",
+                next: ".glider-next",
+            },
 
+            responsive: [
+                {
+                    breakpoint: 200,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                },
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 1,
+                    },
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 2,
+                    },
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3,
+                    },
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 4,
+                    },
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 6,
+                    },
+                },
+            ],
+        })
+    })
