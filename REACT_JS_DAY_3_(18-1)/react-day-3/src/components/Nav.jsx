fea3c4d7c2e96f07a4d87c8195aeb9a7e2d7d8d8
StutiLad <ladstuti18@gmail.com>
2023-01-18 10:03:01 +0530

    commit

---
+++ REACT_JS_DAY_3_(18-1)/react-day-3/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class nav extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <div className="container-fluid justify-content-center mt-3">
+                        <div className="navbar-brand text-center ">
+                            <h3>React JS Day-3</h3>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+            </>
+        )
+    }
+}
+
