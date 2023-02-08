71446a5bce9ccb3be4249ac6346f76ca07a114f0
StutiLad <ladstuti18@gmail.com>
2023-02-08 18:24:17 +0530

    commit

--- .Web Assignments/Assignment2/js/main.js
+++ .Web Assignments/Assignment2/js/main.js
@@ -3,25 +3,65 @@ let nav = document.getElementById('nav');
 nav.style.backgroundColor = 'transparent';
 window.onscroll = function () {
     let scroll = window.pageYOffset;
-    if (scroll < 550) {
+    if (scroll < 500) {
         nav.style.backgroundColor = "transparent";
-    } else if (scroll >= 550) {
+    } else if (scroll >= 500) {
         nav.style.backgroundColor = 'black';
     }
 }
 
 $('.image-slider').slick({
+    dots: true,
     autoplay: true,
     autoplaySpeed: 50000,
 });
 
 $('.testimonial-card').slick({
-    dots:true,
+    dots: true,
     centerMode: true,
     slidesToShow: 3,
     variableWidth: true
-  });
+});
 
-  $('.follow-slider').slick({
-    slidesToShow: 6
-  });
+$('.follow-slider').slick({
+    slidesToShow: 6,
+    responsive: [
+        {
+            breakpoint: 200,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+            },
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 6,
+            },
+        },
+    ],
+});
