221e11e0bd388301e40e5ff222130116fefd3533
StutiLad <ladstuti18@gmail.com>
2023-02-14 18:41:23 +0530

    commit

--- .Web Assignments/Assignment3/js/main.js
+++ .Web Assignments/Assignment3/js/main.js
@@ -2,9 +2,9 @@ let nav = document.getElementById('top-nav');
 
 window.onscroll = function () {
     let scroll = window.pageYOffset;
-    if (scroll < 550) {
+    if (scroll < 400) {
         nav.style.display = '';
-    } else if (scroll >= 550) {
+    } else if (scroll >= 400) {
         nav.style.display = 'none';
     }
 }
